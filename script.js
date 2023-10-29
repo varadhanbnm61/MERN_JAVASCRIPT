d60ae479f7db5ce8acaa11e0670b3ffcc54a8e1e
varadhanbnm61 <varadhanbnm61@gmail.com>
2023-10-29 02:49:50 -0700

    Functions

---
+++ script.js
@@ -0,0 +1,70 @@
+//Step 1:
+//create a XHR object.
+//var request=new XMLHttpRequest();
+// //step2
+// //open a connection to the server
+// request.open("GET","https://restcountries.com/v3.1/all",true)
+// //step3
+// //initiating a bridge to the server
+// request.send();//used to send a request to the server
+// //step4:
+// //server response
+// request.onload=function()
+// {
+// var data=request.response;
+// //console.log(data);
+// var result=JSON.parse(data);
+// //console.log(result);
+// console.log(result[0].name.common);
+
+
+
+// //console.log(result[0].area)
+// //for accessing multiple values use looping
+// for(var i=0;i<result.length;i++)
+// {
+//    console.log(result[i].flags.svg)
+// }
+// }
+
+
+// var a=5;
+// var b=6;
+// console.log(a+b);
+
+// var a1=5;
+// var b1=9;
+// console.log(a1+b1);
+//------
+//function example:
+// function add(a,b)
+// {
+// console.log(a+b);
+// }
+// add(5,6);
+// //---
+// function mul(a,b){
+//    console.log(a*b);
+// }
+// mul(5,6);
+// //--
+// var num1=10;
+// var num2=20;
+// function mul(a,b)
+// {
+//    console.log(a*b);
+// }
+// mul(num1,num2);
+
+
+
+//function always have return
+//return-to reduce the number of time code executed
+
+function add(a,b){
+
+
+}
+//function name is userdefined
+
+//DRY-do not repeat yourself
