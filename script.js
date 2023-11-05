465c823628e28b088e4e2ce06f78c3d4e8ec5286
varadhanbnm61 <varadhanbnm61@gmail.com>
2023-11-04 20:00:20 -0700

    withmap_normal_function

--- script.js
+++ script.js
@@ -4,8 +4,20 @@ var arr=[1,2,3,4,5,6]
 var result=[];
 for(var i=0;i<arr.length;i++){
 
-    arr[i]=arr[i]*2;
-    result.push(arr[i]);
+  //  arr[i]=arr[i]*2;
+    result.push(arr[i]*2);
 
 }
 console.log(result);
+
+//with map-normal function
+ 
+const ar=[2,5,6,78,9];
+function double(element){
+
+    return element*2;
+
+}
+
+const result1=ar.map(double)
+console.log(result1);
