4e4f00d90e3b692814b1990a26a1df1d79070923
varadhanbnm61 <varadhanbnm61@gmail.com>
2023-11-04 19:50:34 -0700

    basicmethod_multiply_each_element

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
-//object destructing:
+//basic example without map:
+//mmultiply each element with array
+var arr=[1,2,3,4,5,6]
+var result=[];
+for(var i=0;i<arr.length;i++){
 
-
-const obj={
-
-    names:"john",
-    age:30,
-    address:{
-        city:"chennai",
-        state:"Tamilnadu"
-    }
+    arr[i]=arr[i]*2;
+    result.push(arr[i]);
 
 }
-
-//without  destructuring:
-
-/*console.log(obj.names);
-console.log(obj.address.city);*/
-
-//with destructuring
-
-//const {key}=objectname;
-
-const{names,age,address:{city}}=obj;
-console.log(names);
-console.log(age);
-console.log(city);
-
+console.log(result);
